639c2872474946fdbf0db38bfada8a7d08cb5346
Martin Bortel <martin.bortel@mavenir.com>
2018-10-07 18:54:41 +0200

    dr:  - global configuration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 ###
 # @Author   http://martinbortel.cz
 # @Contact  martin.bortel@gmail.com
@@ -7,56 +7,65 @@
 #
 # @Package  Distribution route
 #
-# * @Copyright (c) 2018, Ing. Martin Bortel
-# * @author Martin Bortel
-# * @project EA (MM2T 2012) at BUT http://vutbr.cz
-# * @version 1.1
+# @Copyright (c) 2018, Ing. Martin Bortel
+# @author Martin Bortel
+# @project EA (MM2T 2012) at BUT http://vutbr.cz
+# @version 1.1
 ###
 
 # setup global vars
 include inc.common
+include pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+pp.pprint(common.config)
+
+print(common.config['dbserver'])
 
 '''
-global $config, $Context, $Content, $Tools;
-/* @var $Context Context *//* @var $Content Content *//* @var $Tools Tools */
-Tools::clearDB();
-Tools::ScriptNameFromURL2Context();
+global config, Context, Content, Tools
+/* @var Context Context *//* @var Content Content *//* @var Tools Tools */
+Tools::clearDB()
+Tools::ScriptNameFromURL2Context()
 
-/** Fetch posted/sent data to server */
-//preneseno do konstruktoru global objektu $Context
-//$Context->POST = $_POST;
-//$Context->REQUEST = $_REQUEST;
+##
+# Fetch posted/sent data to server
+# preneseno do konstruktoru global objektu Context
+# Context.POST = _POST
+# Context.REQUEST = _REQUEST
 
-/** Prepare content to be displayed */
-if(isset($Context->REQUEST['src']) || isset($Context->REQUEST['path'])){
-    $Content->Dest = $Context->REQUEST['src'];
-}else{
+##
+# Prepare content to be displayed
+if ( isset(Context.REQUEST['src']) || isset(Context.REQUEST['path'])){
+    Content.Dest = Context.REQUEST['src']
+} else {
     //jsme tu prvne, smaz nepatricnosti z db..
-    $Context->Data['Data'] = "";
-    $Context->Save();
+    Context.Data['Data'] = ""
+    Context.Save()
     //presmeruj na spravnou stranku
-    $Tools->Redirect("{$config['baseurl']}?src=run".$Context->getPath());
+    Tools.Redirect("{config['baseurl']}?src=run".Context.getPath())
 }
 
-$Content->GetContent();
+Content.GetContent()
 
-//save Context to db
-$JsonData = array(
-        "BestPopulationFitness" => json_encode($Context->BestPopulationFitness)
-        ,"BestChromosomeFitness" => json_encode($Context->BestChromosomeFitness)
-        ,"ChromosomeFitnessDecay" => json_encode($Context->ChromosomeFitnessDecay)
-        ,"PopulationFitnessDecay" => json_encode($Context->PopulationFitnessDecay)
-);
-$Context->Data['Data'] = json_encode($JsonData);
-$Context->Save();
+# save Context to db
+JsonData = {
+        "BestPopulationFitness" : json_encode(Context.BestPopulationFitness)
+        ,"BestChromosomeFitness" : json_encode(Context.BestChromosomeFitness)
+        ,"ChromosomeFitnessDecay" : json_encode(Context.ChromosomeFitnessDecay)
+        ,"PopulationFitnessDecay" : json_encode(Context.PopulationFitnessDecay)
+}
+Context.Data['Data'] = json_encode(JsonData)
+Context.Save()
 
-/** Prepare parameters of the page, on which data is to be displayed */
-$Page->PageTemplate = "index.html";
-$Page->Debug = false;
+##
+# Prepare parameters of the page, on which data is to be displayed
+Page.PageTemplate = "index.html"
+Page.Debug = false
 
-/** Render the Page to browser */
-echo $Page->Render();
+##
+# Render the Page to browser
+print(Page.Render())
 
-/** end */
-?>
+## END
 '''
